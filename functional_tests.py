9a8a1a94f0906df802be94b775d49a3459629870
liwenli <steliwenli@163.com>
2018-09-18 18:58:53 +0800

    test

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,23 @@
 from selenium import webdriver
-browser =webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Welcome to Django' in browser.title
+import unittest
+
+
+# browser =webdriver.Firefox()
+# browser.get('http://localhost:8000')
+# assert 'to do' in browser.title
+
+class NewVistorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('finish the test')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
