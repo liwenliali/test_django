fc0937c2fa44b14c87df369942142319c3d90edd
liwenli <steliwenli@163.com>
2018-09-19 20:28:20 +0800

    添加django app

--- functional_tests.py
+++ functional_tests.py
@@ -16,7 +16,7 @@ class NewVistorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do',self.browser.title)
+        self.assertIn('Welcome to Django',self.browser.title)
         self.fail('finish the test')
 
 if __name__ == '__main__':
